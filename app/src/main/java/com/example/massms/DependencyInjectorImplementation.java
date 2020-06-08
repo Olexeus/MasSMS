6717c759a994a02ab88a413568f470cc3e045bc0
Zach Peterson <zachpeterson13@icloud.com>
2020-06-07 23:55:46 -0500

    Implement MVP part 1:
    Added BasePresenter and BaseView interfaces
    Added DependencyInjector interface and class (Not sure if we need this)

---
+++ app/src/main/java/com/example/massms/DependencyInjectorImplementation.java
@@ -0,0 +1,6 @@
+package com.example.massms;
+
+public class DependencyInjectorImplementation implements DependencyInjector {
+    // Not sure if we really have dependencies that need to be injected
+    // Examples of dependencies would be a database or a repository of some kind
+}
