bb7b00943d4749c34456fb99aefe5e98aff6178f
Guilherme Faccinetto <guilhermeffaccinetto@gmail.com>
2020-06-23 18:52:21 -0600

    Added methods to DataManager, and added try and catch.

--- app/src/main/java/com/example/massms/models/DataManager.java
+++ app/src/main/java/com/example/massms/models/DataManager.java
@@ -1,6 +1,59 @@
-package com.example.massms.models;
+package com.example.massms;
 
-public class DataManager {
-    // Here we will store any data our app will use
-    // since out app is so small we probably only need one "DataManager" type class
+
+public class DataManager{
+/*
+    public void createDirectory() {
+        File directory;
+        if (filename.isEmpty()) {
+            directory = getFilesDir();
+        } else {
+            directory = getDir(filename, MODE_PRIVATE);
+        }
+        File[] files = directory.listFiles();
+    }
+
+    public void readFile(){
+        //openFileInput reads data from internal storage.
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(filename);
+            Scanner scanner = new Scanner(fis);
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if (scanner != null){
+                scanner.close();
+            }
+        }
+
+        //scanner.close();
+    }
+    public void writeToFile() {
+        FileOutputStream fos;
+        //openFileOutput writes data to internal storage.
+        try {
+            fos = openFileOutput(filename, Context.MODE_PRIVATE);
+            fos.write(internalStorageBinding.saveFileEditText.getText().toString().getBytes());
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if (fos != null){
+               try{
+                   fos.close();
+               }catch (IOException e){
+                   e.printStackTrace();
+               }
+            }
+        }
+
+        //fos.close();
+    }
+    */
 }
