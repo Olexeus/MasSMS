37053ad66b9ecf210c060eb8f86c7450f3760180
Guilherme Faccinetto <guilhermeffaccinetto@gmail.com>
2020-06-30 12:23:51 -0600

    Changes to DataManager and deleted Data

--- app/src/main/java/com/example/massms/models/DataManager.java
+++ app/src/main/java/com/example/massms/models/DataManager.java
@@ -1,43 +1,55 @@
 package com.example.massms;
 
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+import static android.content.Context.MODE_PRIVATE;
+
 public class DataManager{
-/*
-    public void createDirectory() {
+    String fileName = "masssms";
+
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    public void createDirectory(Context context) {
         File directory;
-        if (filename.isEmpty()) {
-            directory = getFilesDir();
+        if (fileName.isEmpty()) {
+            directory = context.getFilesDir();
         } else {
-            directory = getDir(filename, MODE_PRIVATE);
+            directory = context.getDir(fileName, MODE_PRIVATE);
         }
         File[] files = directory.listFiles();
     }
 
-    public void readFile(){
+    public void readFile(Context context){
         //openFileInput reads data from internal storage.
         FileInputStream fis = null;
+        Scanner scanner = null;
         try {
-            fis = openFileInput(filename);
-            Scanner scanner = new Scanner(fis);
+            fis = context.openFileInput(fileName);
+            scanner = new Scanner(fis);
             scanner.useDelimiter("\\Z");
             String content = scanner.next();
         }catch (FileNotFoundException e){
             e.printStackTrace();
         }catch (IOException e){
             e.printStackTrace();
-        }finally {
-            if (scanner != null){
-                scanner.close();
-            }
         }
 
-        //scanner.close();
+        scanner.close();
     }
-    public void writeToFile() {
-        FileOutputStream fos;
+   /* public void writeToFile(Context context, internalStorageBinding) {
+        FileOutputStream fos = null;
         //openFileOutput writes data to internal storage.
         try {
-            fos = openFileOutput(filename, Context.MODE_PRIVATE);
+            fos = context.openFileOutput(fileName, MODE_PRIVATE);
             fos.write(internalStorageBinding.saveFileEditText.getText().toString().getBytes());
         }catch (FileNotFoundException e){
             e.printStackTrace();
@@ -45,15 +57,14 @@ public class DataManager{
             e.printStackTrace();
         }finally {
             if (fos != null){
-               try{
-                   fos.close();
-               }catch (IOException e){
-                   e.printStackTrace();
-               }
+                try{
+                    fos.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
             }
         }
 
-        //fos.close();
-    }
-    */
+    }/*/
+
 }
