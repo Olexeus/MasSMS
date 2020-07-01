3bf8777d99ef2d9b6224c2dc165271d85cfa651b
Olexeus <fedorenkoalex23@gmail.com>
2020-07-01 11:50:17 -0500

    New model class

--- app/src/main/java/com/example/massms/main/ListPresenter.java
+++ app/src/main/java/com/example/massms/main/ListPresenter.java
@@ -1,18 +1,11 @@
 package com.example.massms.main;
 
-import com.example.massms.models.Group;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class ListPresenter implements ListContract.Presenter {
     ListContract.View view;
-    private List<Group> groups;
 
     // Constructor takes in a view, along with any other dependencies
     public ListPresenter(ListContract.View view) {
         this.view = view;
-        this.groups = new ArrayList<Group>();
     }
 
     // Called by the view when it is created
@@ -23,56 +16,5 @@ public class ListPresenter implements ListContract.Presenter {
     // Called by the view when it is destroyed
     @Override
     public void onDestroy() {
-    }
-
-    // getters
-    public List<Group> getGroups() {
-        return groups;
-    }
-
-    public Group getGroup(String name) {
-        // TODO: needs testing
-        int groupIndex = this.findGroup(name);
-        /*if (groups.size() > groupIndex) {
-            return groups.get(groupIndex);
-        }*/
-        if (groupIndex != -1) {
-            return groups.get(groupIndex);
-        }
-        return null;
-    }
-
-    public int findGroup(String name) {
-        for (Group group : groups) {
-            if (group.getFileName().equals(name) || group.getGroupName().equals(name)){
-                return groups.indexOf(group);
-            }
-        }
-        return -1;
-    }
-
-    // setters
-    public void addGroup(Group newGroup) {
-        groups.add(newGroup);
-    }
-
-    public void addGroups(List<Group> newGroups) {
-        groups.addAll(newGroups);
-    }
-
-    // interact with memory
-    public void deleteGroup(String name) {
-        // TODO: delete group even in local memory
-        // TODO: needs testing
-        groups.remove(this.getGroup(name));
-    }
-
-    public void saveGroups() {
-        // TODO: finish saving into memory when DataManager is done
-        // TODO: pass class by reference for higher level of abstraction
-    }
-
-    public void retrieveGroups() {
-
     }
 }
--- app/src/main/java/com/example/massms/models/DataManager.java
+++ app/src/main/java/com/example/massms/models/DataManager.java
@@ -1,4 +1,4 @@
-package com.example.massms;
+package com.example.massms.models;
 
 
 import android.annotation.TargetApi;
@@ -8,7 +8,6 @@ import android.os.Build;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
