b8a5d995ad0b10253307d38a26c090f3cf8370e1
Ben Black <benblack@byui.edu>
2020-07-06 16:56:41 -0600

    Added logic to handle long messages

--- app/src/main/java/com/example/massms/main/SendPresenter.java
+++ app/src/main/java/com/example/massms/main/SendPresenter.java
@@ -9,9 +9,10 @@ import android.util.Log;
 import androidx.core.content.ContextCompat;
 
 import com.example.massms.models.Group;
-import com.example.massms.models.GroupManager;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SendPresenter implements SendContract.Presenter {
     private SendContract.View view;
@@ -20,9 +21,24 @@ public class SendPresenter implements SendContract.Presenter {
 
     @Override
     public void sendMessage(String message, Group group, Context context) {
-        // TODO: Add logic to split message up if it's longer than 160 chars
+        if(message.length() > 160){
+            sendLongMessage(message, group, context);
+        }
+        else{
+            sendShortMessage(message, group, context);
+        }
+    }
+
+    private boolean checkPermission(Context context){
+        int check = ContextCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS);
+        return (check == PackageManager.PERMISSION_GRANTED);
+    }
+    private void sendShortMessage(String message, Group group, Context context){
+
+        // Sends to each contact in the group
         for(int i = 0; i < group.getContacts().size(); i++)
         {
+            // We have to use BigDecimal because floats store in scientific notation
             String phoneNumber = (new BigDecimal(group.getContact(i).getPhone())).toString();
 
             if(phoneNumber.length() == 0 || message.length() == 0){
@@ -30,8 +46,8 @@ public class SendPresenter implements SendContract.Presenter {
                 return;
             }
 
+            // Checks permissions once again
             if(checkPermission(context)){
-
                 Log.d("Sending Message", "Sending to: " + phoneNumber + " and message: "
                         + message);
                 SmsManager smsManager = SmsManager.getDefault();
@@ -40,12 +56,41 @@ public class SendPresenter implements SendContract.Presenter {
                 Log.d("Permission Denied", "DENIED");
             }
         }
-
     }
+    private void sendLongMessage(String message, Group group, Context context){
+        // Splits the message into a List of 160 characters each
+        List<String> longMessage = new ArrayList<>();
+        for(int i = 0; i < message.length(); i += 160){
+            longMessage.add(message.substring(i, Math.min(i + 160, message.length())));
+        }
+        for (int i = 0; i < longMessage.size(); i++) {
+            Log.d("Outputting Message:", longMessage.get(i));
+        }
 
-    private boolean checkPermission(Context context){
-        int check = ContextCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS);
-        return (check == PackageManager.PERMISSION_GRANTED);
+        // Sends to each contact in the group
+        for(int i = 0; i < group.getContacts().size(); i++)
+        {
+            // We have to use BigDecimal because floats store in scientific notation
+            String phoneNumber = (new BigDecimal(group.getContact(i).getPhone())).toString();
+
+            if(phoneNumber.length() == 0 || longMessage.size() == 0){
+                Log.d("Blank values", phoneNumber + " " + longMessage);
+                return;
+            }
+
+            // Checks permissions once again
+            if(checkPermission(context)){
+                // Sends each 160 character string in the message
+                for(int j = 0; j < longMessage.size(); j++){
+                    Log.d("Sending Message", "Sending to: " + phoneNumber + " and message: "
+                            + longMessage.get(j));
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneNumber, null, longMessage.get(i), null, null);
+                }
+            }else{
+                Log.d("Permission Denied", "DENIED");
+            }
+        }
     }
 
     @Override
